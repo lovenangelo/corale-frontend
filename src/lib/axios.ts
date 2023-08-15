dc1b2f14627f983bbc78f3444b8ee97f448c3b7b
Loven Angelo <lovenangelo.dev@gmail.com>
2023-08-15 14:08:01 +0800

    chore: debug

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 import https from "https";
 
 const axiosClient = axios.create({
-  httpsAgent: new https.Agent({
-    rejectUnauthorized: false,
-  }),
+  // httpsAgent: new https.Agent({
+  //   rejectUnauthorized: false,
+  // }),
   baseURL: "http://206.189.147.71/ecommerce/coral-ecommerce-api/public",
   headers: {
     common: {
