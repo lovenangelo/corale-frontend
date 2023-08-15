80c999dc10ee888e4fc57560cf92ea33f26e562f
Loven Angelo <lovenangelo.dev@gmail.com>
2023-08-15 14:09:30 +0800

    debug

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,10 +1,6 @@
 import axios from "axios";
-import https from "https";
 
 const axiosClient = axios.create({
-  // httpsAgent: new https.Agent({
-  //   rejectUnauthorized: false,
-  // }),
   baseURL: "http://206.189.147.71/ecommerce/coral-ecommerce-api/public",
   headers: {
     common: {
