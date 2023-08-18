867d8f4199d18b553216a4041401db4bde41a967
Loven Angelo <lovenangelo.dev@gmail.com>
2023-08-18 08:49:34 +0800

    chore: changed baseurl

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosClient = axios.create({
   // baseURL: "http://206.189.147.71/ecommerce/coral-ecommerce-api/public",
-  baseURL: "https://sea-turtle-app-pothv.ondigitalocean.app",
+  baseURL: "https://sea-turtle-app-pothv.ondigitalocean.app/app",
+  // baseURL: "http://localhost:8000",
   headers: {
     common: {
       Accept: "application/json",
